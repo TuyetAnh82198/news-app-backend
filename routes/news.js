c788e059f8647eb9b98974191afc3e1d9941d968
Tuyết Anh <tuyetanh82198@gmail.com>
2024-03-03 09:31:05 +0700

    Update news.js

--- routes/news.js
+++ routes/news.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const { getNews, search } = require("../controllers/news.js");
+const isAuth = require("../middleware/isAuth.js");
 
 const route = express.Router();
 
 route.post("/get", getNews);
-route.post("/search", search);
+route.post("/search", isAuth, search);
 
 module.exports = route;
